b752a35d0e12f4858df323d6628088e10a7cd98e
Marcos Cunha <marcos.cunha1025@gmail.com>
2025-11-28 07:45:54 -0300

    Atualizado títulos da tabela

--- src/main/resources/static/dist/TemperoDoSertao/server/angular-app-manifest.mjs
+++ src/main/resources/static/dist/TemperoDoSertao/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 5363, hash: '9c90bb5bdefd307f725cfe9de777e5c89bfade9a070731e5dde9b38368771b7b', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1335, hash: 'a7899841b6aff80e0751d2ee87abe77bfd59e265cbd42a16867d570d277c412b', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 5410, hash: '4966f74406a80628dfab28ff27d5b5e07d7b99694b0a63eb1836f0477a3a8e8f', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 5363, hash: '0671e039399fdf706a82401c958e19ce8dd757047058b83cd5e4b43c7c488341', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1335, hash: '76ddd85b5427401546a68406f0fedea8ee0330ac2d90f7ef9abdac98048fc5d9', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 5410, hash: 'b584db97dbff5aa0470cd64b3a9d5f04143b327e77ee5b16cb992884124bd71f', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-HR6D4WJL.css': {size: 315730, hash: 'q9j6xvPSaNQ', text: () => import('./assets-chunks/styles-HR6D4WJL_css.mjs').then(m => m.default)}
   },
 };
--- src/main/resources/static/dist/TemperoDoSertao/server/assets-chunks/index_server_html.mjs
+++ src/main/resources/static/dist/TemperoDoSertao/server/assets-chunks/index_server_html.mjs
@@ -12,6 +12,6 @@ export default `<!doctype html>
 </script>
   <app-root></app-root>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-<link rel="modulepreload" href="chunk-RYZJFD6C.js"><link rel="modulepreload" href="chunk-O24U6QX5.js"><script src="polyfills-5CFQRCPP.js" type="module"></script><script src="main-3MPS4JJM.js" type="module"></script></body>
+<link rel="modulepreload" href="chunk-RYZJFD6C.js"><link rel="modulepreload" href="chunk-O24U6QX5.js"><script src="polyfills-5CFQRCPP.js" type="module"></script><script src="main-TTJOH4SJ.js" type="module"></script></body>
 </html>
 `;
