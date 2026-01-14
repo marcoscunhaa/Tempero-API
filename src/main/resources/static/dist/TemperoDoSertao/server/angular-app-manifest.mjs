7a079519c31d8d6e70fbac89c9031b9095b6cd3a
Marcos Cunha <marcos.cunha1025@gmail.com>
2026-01-14 16:46:56 -0300

    Implementado barra de pesquisa e ordenção alfabética nas tabelas

--- src/main/resources/static/dist/TemperoDoSertao/server/angular-app-manifest.mjs
+++ src/main/resources/static/dist/TemperoDoSertao/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 5362, hash: 'af8d4eb74645ec4501211c7c0bfb72ea78427e0532007f3e55efc9d7972103e7', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1334, hash: 'e157edc2e828a72cdf0f530ec1850f0ceae06a61b35055f9c9b662a2fe5a9d78', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 5409, hash: '4f0d00caf74fe451560d1b9a0b15761b44803b4c8719ffc9c937f2a2c7f89da9', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 5362, hash: '8e60e14eff96598ea4962f9b4df92d3c69efe214a5fbd5359f214753cd583946', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1334, hash: 'f200eb9516f20d15581f27a1f16f024aa1a3b5ad8132e92a20275cbac9664652', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 5409, hash: '6efbfc7014ab433b1aef461cd22a044427d93816f788a310b0741612152f178a', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-HR6D4WJL.css': {size: 315730, hash: 'q9j6xvPSaNQ', text: () => import('./assets-chunks/styles-HR6D4WJL_css.mjs').then(m => m.default)}
   },
 };
--- src/main/resources/static/dist/TemperoDoSertao/server/assets-chunks/index_server_html.mjs
+++ src/main/resources/static/dist/TemperoDoSertao/server/assets-chunks/index_server_html.mjs
@@ -12,6 +12,6 @@ export default `<!doctype html>
 </script>
   <app-root></app-root>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-<link rel="modulepreload" href="chunk-67HXG36L.js"><link rel="modulepreload" href="chunk-RZ7UGWQV.js"><script src="polyfills-5CFQRCPP.js" type="module"></script><script src="main-T5YAR5TG.js" type="module"></script></body>
+<link rel="modulepreload" href="chunk-67HXG36L.js"><link rel="modulepreload" href="chunk-RZ7UGWQV.js"><script src="polyfills-5CFQRCPP.js" type="module"></script><script src="main-JQJXOZXA.js" type="module"></script></body>
 </html>
 `;
